9df0823f9a7630c6268ded399f504da75f74cec4
xyz666 <504578726@qq.com>
2018-06-02 15:58:52 +0800

    处理Less 编译 && dashboard

--- src/component/BreadcrumbCustom.js
+++ src/component/BreadcrumbCustom.js
@@ -4,11 +4,10 @@ import {Breadcrumb} from 'antd'
 
 export default class BreadcrumbCustom extends Component {
     render() {
-        console.log(this.props)
-        // const route=this.props.location.pathname.split('/').filter((i)=>i)
+        const route = this.props.pathname.split('/').filter((i) => i)
         return (
             <Breadcrumb style={{margin: '16px 0'}}>
-                {/*{route&&route.map(item=><Breadcrumb.Item>{item}</Breadcrumb.Item>)}*/}
+                {route && route.map(item => <Breadcrumb.Item key={item}>{item}</Breadcrumb.Item>)}
             </Breadcrumb>
         )
     }
--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,13 +1,48 @@
 import React, {Component} from 'react'
+import {Col, Row, Card,Icon} from 'antd'
 import BreadcrumbCustom from '../component/BreadcrumbCustom'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
-                <BreadcrumbCustom/>
-                <div style={{padding: 24, background: '#fff', minHeight: 360}}>
-                    Bill is a cat.
+                <BreadcrumbCustom {...this.props.location}/>
+                <div>
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Row gutter={16}>
+                                <Col span={12}>
+                                    <Card bordered={false} >
+                                        <div>
+                                            <Icon type="heart" className="text-2x text-danger"/>
+                                            <span className="">520</span>
+                                        </div>
+                                    </Card>
+                                </Col>
+                                <Col span={12}>
+                                    <Card bordered={false} >
+                                        <Icon type="heart" className="text-2x"/>
+                                        <span className="">520</span>
+                                    </Card>
+                                </Col>
+                                <Col span={12}>
+                                    <Card bordered={false} >
+                                        <div>
+                                            <Icon type="heart" className="text-2x"/>
+                                            <span className="">520</span>
+                                        </div>
+                                    </Card>
+                                </Col>
+                                <Col span={12}>
+                                    <Card bordered={false} >
+                                        <Icon type="heart" className="text-2x"/>
+                                        <span className="">520</span>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col span={16}>col-8</Col>
+                    </Row>
                 </div>
             </div>
         )
